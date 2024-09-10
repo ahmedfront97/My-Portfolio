a86a1f53cc86f4f90567a9c15de4c7a677864828
unknown <letxgtd97@gmail.com>
2024-09-10 15:06:45 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 let up_icon = document.getElementById('up-icon');
+let show_mobile_nav_icon = document.getElementById('show-mobile-nav-icon');
+let mobile_nav = document.getElementById('mobile-nav');
+let all_li = document.querySelectorAll('.mobile-nav li')
 
+
+show_mobile_nav_icon.onclick = function(){
+  mobile_nav.classList.toggle('show-mobile-nav')
+}
+
+
+
+
+
+
+function myFunc (){
+  let x ;
+  for(let i = 0 ; i < all_li.length ; i++){
+    console.log(all_li[i]);
+    all_li[i].onclick = function(){
+      mobile_nav.classList.remove('show-mobile-nav')
+    }
+  }
+  
+  
+}
+myFunc ();
 
 // show up icon when scroll > 800px 
 
